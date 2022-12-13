52f24b473d3091e1e59256e6e41ad85b0c495699
Rasathurai Karan <73582838+rasathuraikaran@users.noreply.github.com>
2022-12-13 11:20:46 +0530

    Firebase connected to app

---
+++ App/lib/Screens/Signup/components/signup_form.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/services.dart';
+import '../../../components/already_have_an_account_acheck.dart';
+import '../../../constants.dart';
+import 'package:provider/provider.dart';
+import '../../../provider/auth.dart';
+import '../../Login/login_screen.dart';
+
+
+
+class SignUpForm extends StatefulWidget {
+  const SignUpForm({Key? key}) : super(key: key);
+  @override
+  State<SignUpForm> createState() => _SignUpFormState();
+}
+
+class _SignUpFormState extends State<SignUpForm> {
+  final GlobalKey<FormState> _formKey = GlobalKey();
+  final _auth=FirebaseAuth.instance;
+  String uemail = " ";
+  String upassword = " ";
+  Map<String, String> _authData = {
+    'email': '',
+    'password': '',
+  };
+
+  var _isLoading = false;
+  final _passwordController = TextEditingController();
+
+  Future<void> _submit() async {
+    print(_formKey.currentState!.validate());
+    FocusScope.of(context).unfocus();
+    final bool isValid = _formKey.currentState!.validate();
+    print("karan");
+var  authResult;
+    print(_authData['email']);
+    try{
+ if (isValid) {
+      _formKey.currentState!.save();
+
+      authResult=await _auth.createUserWithEmailAndPassword(email: uemail!.trim(), password: upassword!.trim());
+      print(uemail);
+      print(upassword);
+    }
+    }
+    on PlatformException catch(err){
+var message="error irukkuda check your credential";
+if(err.message!=null) message=err.message!;
+ScaffoldMessenger.of(context).showSnackBar(SnackBar(content:Text(message),backgroundColor: Theme.of(context).errorColor,));
+
+    }
+    catch (err){
+      print(err);
+    }
+   
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          TextFormField(
+            validator: (value) {
+              if (value!.isEmpty || !value.contains('@') || value == null) {
+                return 'Invalid email!';
+              }
+              return null;
+            },
+            keyboardType: TextInputType.emailAddress,
+            textInputAction: TextInputAction.next,
+            cursorColor: kPrimaryColor,
+            onSaved: (value) {
+              uemail = value!;
+            },
+            decoration: InputDecoration(
+              hintText: "E-Mail",
+              prefixIcon: Padding(
+                padding: const EdgeInsets.all(defaultPadding),
+                child: Icon(Icons.person),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: defaultPadding),
+            child: TextFormField(
+              validator: (value) {
+                if (value!.isEmpty || value.length < 5 || value == null) {
+                  return 'Password is too short!';
+                }
+              },
+              onSaved: (value) {
+                upassword = value!;
+              },
+              textInputAction: TextInputAction.done,
+              obscureText: true,
+              cursorColor: kPrimaryColor,
+              decoration: InputDecoration(
+                hintText: "Password",
+                prefixIcon: Padding(
+                  padding: const EdgeInsets.all(defaultPadding),
+                  child: Icon(Icons.lock),
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(height: defaultPadding / 2),
+          ElevatedButton(
+            onPressed: _submit,
+            child: Text("Sign Up".toUpperCase()),
+          ),
+          const SizedBox(height: defaultPadding),
+          AlreadyHaveAnAccountCheck(
+            login: false,
+            press: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) {
+                    return LoginScreen();
+                  },
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ App/lib/provider/auth.dart
@@ -0,0 +1,49 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+import 'package:flutter/widgets.dart';
+import 'dart:convert';
+
+import 'package:flutter/widgets.dart';
+import 'package:http/http.dart' as http;
+
+class Auth with ChangeNotifier {
+  late String _token;
+  late DateTime _expiryDate;
+  late String _userId;
+
+  Future<void> _authenticate(
+      String email, String password, String urlSegment) async {
+    final url = Uri.parse(
+        'https://www.googleapis.com/identitytoolkit/v3/relyingparty/$urlSegment?key=AIzaSyC_QEiHqaRQystjpgJOn6-ZJzEDcIhU65A');
+    try {
+      final response = await http.post(
+        url,
+        body: json.encode(
+          {
+            'email': email,
+            'password': password,
+            'returnSecureToken': true,
+          },
+        ),
+      );
+      print("Karan");
+      print(json.decode(response.body));
+      final responsebody = json.decode(response.body);
+      if (responsebody['error'] != null) {
+        throw HttpException(responsebody['error']['message']);
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  Future<void> signup(String email, String password) async {
+    return _authenticate(email, password, 'signupNewUser');
+  }
+
+  Future<void> login(String email, String password) async {
+    return _authenticate(email, password, 'verifyPassword');
+  }
+}
