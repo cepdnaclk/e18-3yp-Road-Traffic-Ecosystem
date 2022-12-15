09319fbbda6cf6ee751eb376a82f779caccfa7d1
Rasathurai Karan <73582838+rasathuraikaran@users.noreply.github.com>
2022-12-15 17:37:00 +0530

    configured the map from given database cordinate

--- App/lib/Screens/map_screen.dart
+++ App/lib/Screens/map_screen.dart
@@ -1,12 +1,16 @@
+import 'dart:convert';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_auth/provider/map_cordinate.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:url_launcher/url_launcher_string.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:http/http.dart' as http;
 
 class MapScreen extends StatefulWidget {
   static String id = 'Map_Screen';
@@ -20,13 +24,42 @@ class _MapScreenState extends State<MapScreen> {
   String locationMessage = "Current Location of the user";
   late String lat = " ";
   late String long;
-  late DatabaseReference _ref;
+
+  final DatabaseReference _dbRef = FirebaseDatabase.instance.ref();
 
   @override
   void initState() {
     super.initState();
     lat = "";
-    _ref = FirebaseDatabase.instance.ref().child("Maps");
+  }
+
+  Future<MapCordinate> _getCodinate() async {
+    print('ok da chellam paa');
+
+    MapCordinate cord = new MapCordinate();
+    cord.lat = " ";
+    cord.long = " ";
+
+    final url = Uri.parse(
+        'https://roadsafe-ab1d9-default-rtdb.firebaseio.com/Locations.json');
+    final response = await http.get(url);
+    print(json.decode(response.body));
+    print('ok da chellam');
+    final extractData = json.decode(response.body) as Map<String, dynamic>;
+
+    extractData.forEach(
+      (key, value) {
+        if (key == 'User1') {
+          cord.lat = value['lat'];
+          cord.long = value['long'];
+        }
+      },
+    );
+
+    return cord;
+
+    // var snapshot = await _dbRef.child("UserDetails/$myUserId").get();
+    //print(snapshot);
   }
 
   Future<Position> _getCurrentLocation() async {
@@ -66,6 +99,23 @@ class _MapScreenState extends State<MapScreen> {
   }
 
   Future<void> _opanMap(String lat, String long) async {
+    final url = Uri.parse(
+        'https://roadsafe-ab1d9-default-rtdb.firebaseio.com/Locations.json');
+    final response = await http.get(url);
+    print(json.decode(response.body));
+    print('ok da chellam');
+    final extractData = json.decode(response.body) as Map<String, dynamic>;
+
+    extractData.forEach(
+      (key, value) {
+        if (key == 'User1') {
+          lat = value['lat'].toString();
+          long = value['long'].toString();
+        }
+      },
+    );
+    print(lat);
+    print(long);
     String googleUrl =
         'https://www.google.com/maps/search/?api=1&query=$lat,$long';
     await canLaunchUrlString(googleUrl)
@@ -130,8 +180,6 @@ class _MapScreenState extends State<MapScreen> {
                   });
                   _liveLocation();
                 });
-
-                _ref.push().set(lat);
               },
               child: Text("Get Current Location")),
           SizedBox(
