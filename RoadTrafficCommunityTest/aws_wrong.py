ae53998fa0dbe469434d8b319eb5bb7b61197820
Hari25483 <haricodmw@gmail.com>
2023-01-04 01:02:41 +0530

    Create aws_wrong.py

---
+++ RoadTrafficCommunityTest/aws_wrong.py
@@ -0,0 +1,27 @@
+import  requests
+
+
+class Response_wrong(object):
+    def __init__(self):
+        self.headers = {
+            'Accept-Encoding': 'gzip, deflate, sdch',
+            'Accept-Language': 'en-US,en;q=0.8',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+            'Referer': 'http://www.wikipedia.org/',
+            'Connection': 'keep-alive'}
+        self.url = " https://13bc-18-179-204-141.ngrok.io/find_nearest_users?longtitude=79.93994&latitude=as"
+
+    @property
+    def get(self):
+        try:
+            r = requests.get(url=self.url, headers= self.headers, timeout=5)
+            if r.ok:
+                return r
+            else:
+                return None
+        except requests.exceptions.Timeout:
+            return 'Bad Response'
+
+resp=Response_wrong()
+print(resp.get)
